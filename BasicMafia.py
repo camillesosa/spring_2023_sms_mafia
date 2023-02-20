1854ceda89cef9567cdf5f4ab6cdd19731b85c40
camillesosa <71521586+camillesosa@users.noreply.github.com>
2023-02-20 15:45:46 -0800

    Update BasicMafia.py

--- BasicMafia.py
+++ BasicMafia.py
@@ -1,38 +1,59 @@
 state = 0
 while (state != 4):
-    if (state == 0):
-        # welcome message
-        print(
-            'Welcome to Mafia! First and foremost, you will be assigned a nickname. Note: From here on out, you will only be identified by your nickname. Once everyone has been assigned a nickname, I will explain how to play the game!\n')
-        # nick name assigner
-        # put in dictionary here
-        # (nickname, phonenumber)
-        # rules
-        print(
-            'Here is how to play the game: A player is randomly selected to be the mafia, and their goal is to kill the citizens without being caught. The citizens goal, which are the rest of you, is to find and arrest the mafia! After each round, the killed player is revealed and the citizens will be allowed to discuss who the mafia is. Once a consensus is reached, one citizen will be asked to state the suspected mafia in the following format: The mafia is [nickname]. If you were right, then the citizens won!  If you were wrong however, the game will move on to the next round. The mafia has won when there is just one villager left standing.\n')
-        state = state + 1
-
     if (state == 1):
-        # message seperately and assign roles
-        assignMafia = 'You are the mafia. You will use this chat to choose which citizen to kill. Please await further instructions.'
-        # for testing
-        mafia = 'Camille'
-        assignCitizen = 'You are a citizen. Good luck and stay safe!'
-        # put citizens in a dynamic array (nicknames)
-        # for testing
-        citizenArray = ['Luis', 'Francesca', 'Camella']
-        # mafia win if array size < 2
-        # choose random mafia, if mafia send assignMafia to number, send assignCitizen to everyone else
-
-        print('The roles have now been assigned, get ready for the first round...')
+        # welcome message
+        name = input('Welcome Detective! Do you mind confirming your name before I go over the details of the case?\n')
+        print('Thank you Detective ', name, ', we are happy to have you on this case. Unfortunately, there seems to be a killer on the loose! We have narrowed the suspects to five individuals.')
+        #suspect names
+        print('These five suspects were guests at a dinner party at Hill House, a secluded mansion in New England, where the murder took place. In an attempt to prevent escape, we have asked all the guests to stay there while we attempt to find the murderer, but the longer we take to find the murderer, the longer the innocents are in danger of also being attacked. I will take you to Hill House, so you can take a look at the evidence.')
+        #randomly assign one suspect to be the murder
+        
+        #For testing
+        murderer = 'Camille'
+        killed = 'Steve'
+        suspects = ['Luis', 'Francesca', 'Camella', 'Camille', 'Victor']
+        # lose if array size < 2
         state = state + 1
 
+        
     if (state == 2):
-        print('Time for everyone to go to sleep...')
+        print('Victim was...')
+        #Randomly pick murder time, murder weapon, and murder location
+        #For example, murder occured sometime after 3 but before 5, victim was killed with a hammer in the gardens
+        
+        #Randomly assign allibis (vague, with some conflicting
+        #For example, Victor was with me in the kitchen at 4, but Victor said he was in the garage
+        #send media files as well
+        
+        voted = "F"
+        while (voted != "T"):
+            text = input
+            # fix this: recognizing text to pull out specific input
+            # if(text[0-13] == 'The murderer is '):
+            # take nickname out of input
+            maybeMafia = input('You think the mafia is... ')
+            voted = "T"
+        state = state + 1
 
-        # send to mafia privately
-        forMafia = 'Who would you like to kill?  Please just text the nickname with no punctuation\n'
-        killed = input(forMafia)
+        
+    if (state == 3):
+       if (maybeMurder != murderer):
+            print("Oh no! ", maybeMurderer, " was not the murderer!")
+            # look up nickname in citizen array, delete entry
+            if (len(suspects) > 1):
+                # move on to next round
+                print('Looks like the murderer is still out there... Hopefully there are no more attacks...')
+                state = 2
+            if (len(suspects < 2):
+                # mafia wins, go to end state
+                print('Oh no! You failed to find the murderer in time :(')
+                state = 4
+
+        if (maybeMafia == mafia):
+            print("Excellent job Detective! ", murderer, " was the murderer!  Congrats, you win!")
+            # win, go to end state
+            state = 4
+
         # look up nickname in citizen array, delete entry
         index = 0
         while (index < len(citizenArray)):
@@ -41,46 +62,14 @@ while (state != 4):
                 index = len(citizenArray)
             else:
                 index = index + 1
-        print('Got it, thanks!\n')
 
-        # to group chat
-        print('Goodmorning! Last night, it seems the mafia has attacked! There are ', (len(citizenArray)),
-              ' of you left! ', killed,
-              ' has been killed :( Someone here must be the mafia! Discuss who you think is the mafia, and once a consensus is reached, someone please state the suspected werewolf in the following format: The mafia is [nickname]')
-        voted = "F"
-        while (voted != "T"):
-            text = input
-            # fix this: recognizing text to pull out specific input
-            # if(text[0-13] == 'The mafia is '):
-            # take nickname out of input
-            youThink = 'You think the mafia is... '
-            maybeMafia = input('You think the mafia is... ')
-            voted = "T"
-        state = state + 1
 
-    if (state == 3):
-        if (maybeMafia != mafia):
-            print("Oh no! ", maybeMafia, " was not the mafia!")
-            # look up nickname in citizen array, delete entry
-            if (len(citizenArray) > 1):
-                # move on to next round
-                print('Looks like the mafia is still out there... Hopefully there are no more attacks...')
-                state = 2
-            if (len(citizenArray) < 2):
-                # mafia wins, go to end state
-                print('Oh no! The mafia has won - congrats mafia!')
-                state = 4
-
-        if (maybeMafia == mafia):
-            print("The villagers are safe! ", mafia, " was the mafia!  Congrats citizens, you have won!")
-            # citizens win, go to end state
-            state = 4
 
     if (state == 4):
         # end state
         # play again question?  maybe make it a vote?
         playAgain = input(
-            'Thanks for playing! Would you like to play again? Note: nicknames would remain the same. yes/no\n')
+            'Thanks for playing! Would you like to play again? yes/no\n')
         if (playAgain == 'yes'):
             state = 1
         if (playAgain == 'no'):
