e02248a9ba6d50326302b50b0822ef01a3103e7b
camillesosa <71521586+camillesosa@users.noreply.github.com>
2023-02-22 14:20:08 -0800

    Update BasicMafia.py

--- BasicMafia.py
+++ BasicMafia.py
@@ -27,12 +27,13 @@ class BasicMafia:
       name = 'Cam'
       # name = input('Welcome Detective! Do you mind confirming your name before I go over the details of the case?\n')
       resp = fn.handle_welcome()
-      logger.debug('Thank you Detective', name,
-            ', we are happy to have you on this case. Unfortunately, there seems to be a killer on the loose! We have narrowed the suspects to five individuals: ')
+      #name = input after handle_welcome
+      logger.debug('Thank you Detective', name, ', we are happy to have you on this case. Unfortunately, there seems to be a killer on the loose! We have narrowed the suspects to five individuals: ')
+      #maybe pass input from handle_welcome into handle_gameRules for name
+      resp = fn.handle_gameRules()
       # suspect names
       logger.debug('Camella, Camille, Francesca, Luis, and Victor.\n')
-      logger.debug(
-        'These five suspects were guests at a dinner party at Hill House, a secluded mansion in New England, where the murder took place. In an attempt to prevent escape, we have asked all the guests to stay there while we attempt to find the murderer, but the longer we take to find the murderer, the longer the innocents are in danger of also being attacked. I will take you to Hill House, so you can take a look at the evidence.')
+      logger.debug('These five suspects were guests at a dinner party at Hill House, a secluded mansion in New England, where the murder took place. In an attempt to prevent escape, we have asked all the guests to stay there while we attempt to find the murderer, but the longer we take to find the murderer, the longer the innocents are in danger of also being attacked. I will take you to Hill House, so you can take a look at the evidence.')
       # randomly assign one suspect to be the murder
 
       # For testing
