2f53ef008f66031d5dd3974c4f6dc3fbf0406b83
KingDTheMC <DavidCoats64@gmail.com>
2023-02-25 12:38:01 -0800

    copy class example

--- players/actors.py
+++ players/actors.py
@@ -1,30 +1,16 @@
-class Actor:
-    def __init__(self,phone_number, name, role):
+import json
+
+Our_Game_Logic = {}
+
+with open('game_chat.json', 'r') as gLFile:
+    Our_Game_Logic = json.loads(gLFile.read())
+
+
+class actor:
+    def __init__(self, phone_number):
         self.phone = phone_number
-        self.prev_msgs=[]
-        self.name = name
-        self.role = role
-    def save_msg(self,msgs):
-        self.prev_msgs.append(msgs)
+        self.prev_msgs = []
+        self.state = "init"
 
-class MafiaGame:
-    def __init__(self, players):
-        self.players = [Actor(player, None) for player in players]
-        
-    def assign_roles(self):
-        number_of_mafiosos = len(self.players) // 3
-        for i in range(number_of_mafiosos):
-            self.players[i].role = "Mafioso"
-        for i in range(number_of_mafiosos, len(self.players)):
-            self.players[i].role = "Innocent"
-            
-    def show_roles(self):
-        for player in self.players:
-            print(f"{player.name} is a {player.role}")
-
-players = [""]
-players.append(Actor.name)
-#game = MafiaGame(players)
-#game.assign_roles()
-#game.show_roles()
-   
+    def save_msg(self, msg):
+        self.prev_msgs.append(msg)
