4c71a82909afd83840699094fa93fa055f1a3790
KingDTheMC <DavidCoats64@gmail.com>
2023-02-25 14:14:54 -0800

    update chat.py file with reponse

--- players/chat.py
+++ players/chat.py
@@ -4,4 +4,27 @@ import random
 
 
 LOGIC = {}
-with open('
+with open('chatbot_comments.json', 'r') as myComm:
+	LOGIC = json.loads(myComm.read())
+	
+
+
+class chat(actor):
+	def __init__(self, phone_number):
+		super().__init__(phone_number)
+		self.score = 0
+		
+	
+	def get_output(self,msg_input):
+		
+		
+		msg = None
+		for i in range( len(LOGIC[ "misc in" ]) ):
+			msg = random.choice( LOGIC[ "misc in" ])
+			if msg not in self.prev_msgs:
+				break
+				
+		if msg == None:
+			return [ random.Choice( LOGIC[ "misc in" ] ) ]
+		else:
+			return [ msg ]
